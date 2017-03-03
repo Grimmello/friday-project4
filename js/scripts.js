1939b8edee4c771c82bf4eb79c1c7d51f555bb54
Clifford Grimmell <agrimmell2@gmail.com>
2017-03-03 11:58:58 -0800

    structure script with comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
 //Back end
+
+//sets up global variables
 var toppingArray = [],
     inputs = document.getElementsByTagName("input"),
     pizzaPrice = 0,
     pizzaCounter = 1;
 
+//pizza object
 function Pizza(sizePizza,sauce,toppings) {
   this.sizePizza = sizePizza
   this.sauce = sauce
   this.toppings = toppings
 }
+
+//checks to see if sauce value is none or marinara;
 function sauceChecker(sauces) {
-  if (sauces === "None") {
-    return 0;
-  } else if (sauces === "Marinara"){
+  if (sauces === "None" || sauces === "Marinara") {
     return 0;
   } else {
     return 0.5;
@@ -36,20 +39,25 @@ $(function () {
     var sizePizza = parseFloat($("#sizePizza :selected").val());
     var sauce = $("#sauce :selected").val();
 
+    // Checks which sauce is selected and will $0.50 if it is anything other than none or marinara
     var check = sauceChecker(sauce);
+
+    // runs array to determine which boxes are checked
     checkboxArray();
 
     var newPizza = new Pizza(sizePizza, sauce, toppingArray);
 
+    // sets pizza price and sets it to 2 decimal places
     pizzaPrice = (pizzaPrice + newPizza.sizePizza + check + (toppingArray.length * 0.5));
-
     var price = Number(pizzaPrice).toFixed(2);
 
     $("#price").text("Total Price: $"+price);
 
     // Adds pizza to list
     $("ul#pizzaList").append("<li><span class='pizza'> Pizza #"+pizzaCounter+"</span> <br>Size: " + newPizza.sizePizza + " in. <br> Sauce: "+newPizza.sauce+" <br> Toppings: "+ toppingArray.join() +"</li>");
+    $("#output").show();
     pizzaCounter = pizzaCounter+1
+
     // Will reset fields for new pizza
     $("#pizza-builder").each(function() {
       this.reset();
